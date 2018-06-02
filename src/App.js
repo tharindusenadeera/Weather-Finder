548e320e7a20920ddcf78ae374451852cfe09aa9
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-06-03 01:58:45 +0530

    Displaying values

--- src/App.js
+++ src/App.js
@@ -25,28 +25,50 @@ class App extends React.Component{
         const country = e.target.elements.country.value;
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
         const data = await api_call.json();
-        console.log(data);
-        this.setState({
-            temperature: data.main.temp,
-            city:data.name,
-            country: data.sys.country,
-            humidity:data.main.humidity,
-            description:data.weather[0].description
-        })
+
+        if (city && country) {
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        }
+        else {
+            console.log(data);
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please Enter the City and Country..."
+            });
+        }
     }
 
-    render(){
+    render() {
         return (
             <div>
 
                 <Titles/>
                 <Form getWeather={this.getWeather}/>
-                <Weather/>
+                <Weather
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+                />
 
             </div>
 
         );
     }
-}
+};
 
 export default App;
